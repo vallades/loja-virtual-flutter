dfdcaae3f9dd51eab0a664f0b4ede67514e82764
Felipe Vallades <vallades@gmail.com>
2020-03-15 23:11:28 -0300

    Initial files

---
+++ loja_virtual/lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        )
+      ],
+    );
+  }
+}
