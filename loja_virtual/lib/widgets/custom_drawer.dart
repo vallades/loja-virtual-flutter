618e69c998688f6494fb842eac00e20547b9e865
Felipe Vallades <vallades@gmail.com>
2020-03-16 22:57:05 -0300

    UI improvements and Add product class.

--- loja_virtual/lib/widgets/custom_drawer.dart
+++ loja_virtual/lib/widgets/custom_drawer.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
+
+  final PageController pageController;
+
+  CustomDrawer(this.pageController);
+
   @override
   Widget build(BuildContext context) {
     Widget _buildDrawerBack() => Container(
@@ -33,9 +39,41 @@ class CustomDrawer extends StatelessWidget {
                             fontSize: 34.0, fontWeight: FontWeight.bold),
                       ),
                     ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Olá,",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              "Entre ou cadastre-se",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            onTap: () {
+
+                            },
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-              )
+              ),
+              Divider(),
+              DrawerTile(Icons.home, "Início", pageController, 0),
+              DrawerTile(Icons.list, "Produtos", pageController, 1),
+              DrawerTile(Icons.location_on, "Lojas", pageController, 2),
+              DrawerTile(Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
             ],
           )
         ],
